cb0de98b900adcba553a62cd8299e0c2d0a9cdae
Joshua Retallick <josh@old-empire.co.uk>
2021-01-12 14:28:31 +0000

    Sanity checked that a longer argument can be passed in as sound_wave and still be returned correctly

--- spec/digital_filter_spec.rb
+++ spec/digital_filter_spec.rb
@@ -2,7 +2,12 @@ require 'digital_filter'
 
 describe "#digital_filter" do
   # [40], 40, 1000                        |       [40] # no change
-  it "should return the original sound wave when highest & lowest frequencies are not reached" do
+  it "should return the original sound wave when highest & lowest frequencies are not reached, with single sound_wave" do
     expect(digital_filter([40], 40, 1000)).to eq [40]
   end
+
+  it "should return the original sound wave when highest & lowest frequencies are not reached, a longer sound_wave" do
+    expect(digital_filter([40, 100, 1000], 40, 1000)).to eq [40, 100, 1000]
+  end
+
 end
