cab5040c559d778b02a2eb06070eb6b54db38063
Joshua Retallick <josh@old-empire.co.uk>
2021-01-12 14:39:19 +0000

    Takes sound_wave input and modifies lowest value if outside of allowed parameters

--- lib/digital_filter.rb
+++ lib/digital_filter.rb
@@ -1,3 +1,15 @@
 def digital_filter(sound_wave, low_freq, high_freq)
-  sound_wave
+
+  new_sound_wave = []
+
+  sound_wave.each do |freq|
+    if freq < low_freq
+      new_sound_wave << low_freq
+    else
+      new_sound_wave << freq
+    end
+  end
+  return new_sound_wave
+
+
 end
